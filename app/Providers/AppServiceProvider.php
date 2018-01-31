8e03a91bbf05dd4b17ed3df72a6843890e8ed05d
nguyenky <lenguyenky1801@gmail.com>
2018-01-31 15:22:46 +0700

    add templete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // dd(url('/'));
-        View::share('url',url('/'));
+        View::share('url',getenv('URL'));
     }
 
     /**
